578eb67f900e5db39eb8a3016537d6ca27892e60
Robyn Arnecke <robynarnecke@gmail.com>
2022-01-04 23:12:30 -0600

    Some style changes, and quiz questions have been found and put in arrays for later use

--- assets/script.js
+++ assets/script.js
@@ -1,12 +1,24 @@
 var startButton = document.querySelector("#start-button");
 var timerElement = document.querySelector("#timer-count");
+var questionElement = document.querySelector(".question-card");
 
+//Q&A arrays
+var questionArray = ["Which tag do we use in HTML for Javascript code/links?", "Which JavaScript method is used to write on browser's console?", "In JavaScript, single line comment begins with what?", "Which property is used to get the length of a string in JavaScript?"];
+var answerArray = [["<script>...</script>", "<div>...</div>", "<body>...</body>", "<p>...</p>"], ["console.write()", "console.output()", "console.log()", "console.writeHTML()"], ["#", "//", "/*", "%"], ["len", "strlen", "stringlength", "length"]];
+
+//Score variables
+var currentScore = 0;
+var highScore = 0;
+
+//Timer variables
 var timerCount;
 var timer; 
 
 function startQuiz() {
     timerCount = 30;
     startTimer();
+    
+    questionElement.textContent = questionArray[0];
 }
 
 function startTimer() {
