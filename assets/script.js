c5564463549f6dea701d62cb7bbe5f1b6d210f23
Robyn Arnecke <robynarnecke@gmail.com>
2022-01-09 18:20:05 -0600

    The question elements now repopulate once the buttons is clicked; added a score card

--- assets/script.js
+++ assets/script.js
@@ -1,8 +1,11 @@
 var startButton = document.querySelector("#start-button");
 var timerElement = document.querySelector("#timer-count");
+var scoreElement = document.querySelector("#score-count");
 var questionElement = document.querySelector(".question-card");
 var answerList = $("#answer-list");
-var a1, a2, a3, a4;
+var a1;
+var index = 0;
+var score = 0;
 var buttonClicked;
 
 //Q&A array
@@ -15,17 +18,20 @@ var questionsArray = [
     {
         question: "Which JavaScript method is used to write on browser's console?",
         options: ["console.write()", "console.output()", "console.log()", "console.writeHTML()"],
-        answer: "console.log()"
+        answer: "console.log()",
+        ready: false
     },
     {
         question: "In JavaScript, single line comment begins with what?",
         options: ["#", "//", "/*", "%"],
-        answer: "//"
+        answer: "//",
+        ready: false
     },
     {
         question: "Which property is used to get the length of a string in JavaScript?",
         options: ["len", "strlen", "stringlength", "length"],
-        answer: "length"
+        answer: "length",
+        ready: false
     }
 ];
 
@@ -38,19 +44,18 @@ var timerCount;
 var timer; 
 
 function startQuiz() {
+    // Start the timer and make it so that it cannot be clicked again
     timerCount = 30;
     startTimer();
     startButton.disabled = true;
     
-    questionElement.textContent = questionsArray[0].question;
+    //First Q: populate question, populate answer buttons, listen for click
+    questionElement.textContent = questionsArray[index].question;
+    populateAnswers(index);
+    //This answerQ function will populate each question until the last one, at which point the field will be cleared
+    answerList.on('click', '.answer-button', answerQ);
 
-    for(var i=0; i<4; i++) {
-        a1 = document.createElement('button');
-        a1.textContent = questionsArray[0].options[i];
-        a1.setAttribute("class", "answer-button");
-        questionElement.appendChild(a1);
-    }
-    
+
 }
 
 function startTimer() {
@@ -59,16 +64,47 @@ function startTimer() {
         timerElement.textContent = timerCount; 
         if(timerCount === 0) {
             clearInterval(timer);
-            startButton.disabled = false;
+            questionElement.innerHTML = 'Time is up! Better luck next time.';
         }
     }, 1000);
 }
 
 function answerQ(event) {
+    if(index == questionsArray.length) {
+        
+        return;
+    }
     buttonClicked = event.target;
     console.log(buttonClicked.textContent);
-    questionElement.innerHTML = '';
+    if(buttonClicked.textContent == questionsArray[index].answer){
+        score++;
+        scoreElement.textContent = score;
+    }
+    console.log(score);
+    
+    //Update the global index and re-populate questions (checking to make sure we don't go past last question object)
+    if(index < questionsArray.length-1){
+        index++;
+        console.log(index);
+        questionElement.textContent = questionsArray[index].question;
+        populateAnswers(index);
+    } else if(index == questionsArray.length-1) {
+        questionElement.textContent = '';
+    }
+}
+
+function populateAnswers(index) {
+    if(index == questionsArray.length) {
+        return;
+    }
+    
+    for(var i=0; i<4; i++) {
+        a1 = document.createElement('button');
+        a1.textContent = questionsArray[index].options[i];
+        a1.setAttribute("class", "answer-button button"+i);
+        questionElement.appendChild(a1);
+    }
 }
 
 startButton.addEventListener('click', startQuiz);
-answerList.on('click', '.answer-button', answerQ);
+// answerList.on('click', '.answer-button', answerQ);
