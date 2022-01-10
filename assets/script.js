bc4ec16e500626e6aa96c38197d774dd5546e7fa
Robyn Arnecke <robynarnecke@gmail.com>
2022-01-09 19:04:35 -0600

    Added a endQuiz function and an input there for initials

--- assets/script.js
+++ assets/script.js
@@ -7,6 +7,8 @@ var a1;
 var index = 0;
 var score = 0;
 var buttonClicked;
+var winFlag = false;
+var initials;
 
 //Q&A array
 var questionsArray = [
@@ -40,12 +42,12 @@ var currentScore = 0;
 var highScore = 0;
 
 //Timer variables
-var timerCount;
+var timerCount = 30;
 var timer; 
 
 function startQuiz() {
     // Start the timer and make it so that it cannot be clicked again
-    timerCount = 30;
+    // timerCounter = 30;
     startTimer();
     startButton.disabled = true;
     
@@ -64,7 +66,7 @@ function startTimer() {
         timerElement.textContent = timerCount; 
         if(timerCount === 0) {
             clearInterval(timer);
-            questionElement.innerHTML = 'Time is up! Better luck next time.';
+            endQuiz();
         }
     }, 1000);
 }
@@ -79,6 +81,9 @@ function answerQ(event) {
     if(buttonClicked.textContent == questionsArray[index].answer){
         score++;
         scoreElement.textContent = score;
+    } else {
+        //5 second penalty for wrong answers
+        timerCount-=5;
     }
     console.log(score);
     
@@ -89,7 +94,10 @@ function answerQ(event) {
         questionElement.textContent = questionsArray[index].question;
         populateAnswers(index);
     } else if(index == questionsArray.length-1) {
-        questionElement.textContent = '';
+        winFlag = true;
+        //Score is questions answered correctly plus the time left, add it before reducing time to trigger endQuiz
+        score+=timerCount;
+        timerCount = 1;
     }
 }
 
@@ -106,5 +114,21 @@ function populateAnswers(index) {
     }
 }
 
+function endQuiz() {
+    if(winFlag) {
+        questionElement.textContent = 'SUCCESS! Good job. Your score is: ' + score + '. Enter your initials to save your score: ';
+    } else {
+        questionElement.textContent = 'Drats! Time is up. Your score is: ' + score + '. Enter your initials to save your score: ';
+    }
+
+    initials = document.createElement('INPUT');
+    initials.setAttribute("type", "text");
+    questionElement.appendChild(initials);
+    var submit = document.createElement('INPUT');
+    submit.setAttribute("type", "submit");
+    questionElement.appendChild(submit);
+
+    console.log(initials.value);
+}
+
 startButton.addEventListener('click', startQuiz);
-// answerList.on('click', '.answer-button', answerQ);
